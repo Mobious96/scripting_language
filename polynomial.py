1e0ba6b00ee05ea86a3ced149077e4ece682585e
Alexander Trifonov <alexander1.trifonov@intel.com>
2020-03-02 13:57:44 +0300

    add, eq, str fix, repr, more tests

--- polynomial.py
+++ polynomial.py
@@ -5,11 +5,14 @@ class Polynomial:
     #self.coeffs = []
 
     def __init__(self,_coeffs):
-        if not isinstance(_coeffs, (list, tuple, set)):
+        self.coeffs = []
+        if isinstance(_coeffs, (int)):
+            self.coeffs.append(_coeffs)
+            return
+        if not isinstance(_coeffs, (list, tuple, set, int)):
             raise TypeError("Supported types for creating polynomial: list, tuple, set")
         badZerosExists = True #remove first N zeros since they are usless, e.g. [0,0,0,1,2] -> x + 2 same as [1,2]
         #index = _coeffs.index(not 0);
-        self.coeffs = []
         for v in _coeffs: #1 2 3 4 -> 1x^3 + 2x^2 + 3x + 4, but coeffs[0] = 4, coeffs[1] = 3, coeffs[i] = arg(x^i)
             if(badZerosExists):
                 if v != 0:
@@ -25,14 +28,42 @@ class Polynomial:
         text = ""
         #print("text:" + text)
         #For X with max degree there are special rule to represent: "-2x^n_+_...". "-" without spaces
-        #x^n
-        text = text + (("-" if self.coeffs[0] < 0 else "") + (str(abs(self.coeffs[0])) if abs(self.coeffs[0]) != 1 else "") + "x^" + str(len(self.coeffs) - 1) if self.coeffs[0] != 0 else "")
-        for k,v in enumerate(self.coeffs[1: -2]):
-            if v != 0:
-                text = text + (" - " if v < 0 else " + ") + (str(abs(v)) if abs(v) != 1 else "") + "x^"+str(len(self.coeffs)-k - 2)
-        #x^1
-        text = text + (((" + " if self.coeffs[-2] > 0 else " - ") + (str(abs(self.coeffs[-2])) if ((abs(self.coeffs[-2]) != 1) or (self.coeffs[-2] != 0)) else "") + "x") if self.coeffs[-2] != 0 else "")
-        #x^0
-        text = text + ((" + " if self.coeffs[-1] > 0 else " - ") + (str(abs(self.coeffs[-1])) if ((abs(self.coeffs[-1]) != 1) or (self.coeffs[-1] != 0)) else "") if self.coeffs[-1] != 0 else "")
+
+        if (len(self.coeffs) > 1):
+            #x^n
+            text = text + (("-" if self.coeffs[0] < 0 else "") + (str(abs(self.coeffs[0])) if abs(self.coeffs[0]) != 1 else "") + "x^" + str(len(self.coeffs) - 1) if self.coeffs[0] != 0 else "")
+            #print("text1:" + text)
+            #[x^(n-1)..x^2]
+            for k,v in enumerate(self.coeffs[1: -2]):
+                if v != 0:
+                    text = text + (" - " if v < 0 else " + ") + (str(abs(v)) if abs(v) != 1 else "") + "x^"+str(len(self.coeffs)-k - 2)
+                    #print("text:"+k+" " + text)
+            #x^1
+            text = text + (((" + " if self.coeffs[-2] > 0 else " - ") + (str(abs(self.coeffs[-2])) if (abs(self.coeffs[-2]) != 1) else "") + "x") if self.coeffs[-2] != 0 else "")
+            #x^0
+            text = text + (((" + " if self.coeffs[-1] > 0 else " - ") + str(abs(self.coeffs[-1]))) if self.coeffs[-1] != 0 else "")
+        else:
+            text = text + ((("" if self.coeffs[-1] > 0 else "-") + str(abs(self.coeffs[-1]))) if self.coeffs[-1] != 0 else "")
         return text
 
+    def __repr__(self):
+        return "Polynomial("+str(self.coeffs)+")"
+
+    def __eq__(self, other):
+        if not isinstance(other, Polynomial):
+            TypeError("Can't compare " + str(type(self)) + " with " + str(type(other)))
+        else:
+            return self.coeffs == other.coeffs
+
+    def __len__(self):
+        return len(self.coeffs)
+
+    def __add__(self, other):
+        if isinstance(other, Polynomial):
+            if len(self) > len(other):
+                for k,v in enumerate(other.coeffs):
+                    self.coeffs[len(self.coeffs) - k - 1] += other.coeffs[len(self.coeffs) - k - 1] #since 1,2,3,4 means x^3 + 2x^2 + 3x^1 + 4 we start from the end
+            else:
+                for k,v in enumerate(self.coeffs):
+                    self.coeffs[len(self.coeffs) - k - 1] += other.coeffs[len(self.coeffs) - k - 1]
+        return self
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import polynomial
 import unittest
 
-class PolynomialTestRepresentation(unittest.TestCase):
+class PolynomialTests(unittest.TestCase):
 
     def test_unsupportedTypes(self):
         with self.assertRaises(Exception):
@@ -12,16 +12,49 @@ class PolynomialTestRepresentation(unittest.TestCase):
         polynomial.Polynomial((1, 2, 3))
         polynomial.Polynomial({1,2,3})
 
+    # [repr and str]
     def test_negativeCoeffs(self):
         self.assertEqual(str(polynomial.Polynomial([1,-2, 3])), "x^2 + 2x + 3")
 
     def test_negativeCoeffs(self):
         self.assertEqual(str(polynomial.Polynomial([-1, -2, -3])), "-x^2 - 2x - 3")
 
-    def test_badZeros(self):
-        self.assertEqual(str(polynomial.Polynomial([0,0,0,1,2,3])), str(polynomial.Polynomial([1,2,3])))
+    def test_repr(self):
+        self.assertEqual(repr(polynomial.Polynomial([1,2,-4])), "Polynomial([1, 2, -4])")
 
-    def test_zeroCoeffs(self):
+    def test_boundMin(self):
+        self.assertEqual(str(polynomial.Polynomial(1)), "1")
+
+    def test_boundMinNegative(self):
+        self.assertEqual(str(polynomial.Polynomial(-1)), "-1")
+
+    def test_boundZeroOnTheEnd(self):
+        self.assertEqual(str(polynomial.Polynomial([1,0,0,0,0,0])), "x^5")
+
+    def test_boundZeroInTheStart(self):
+        self.assertEqual(str(polynomial.Polynomial([0, 0, 0, 0, 0, 1])), "1")
+
+    def test_zeroCoeffsMixed(self):
         self.assertEqual(str(polynomial.Polynomial([1, 0, 0, 4])), "x^3 + 4")
 
+    def test_boundMinZero(self):
+        self.assertEqual(str(polynomial.Polynomial(0)), "")
+
+    def test_badZeros(self):
+        self.assertEqual(str(polynomial.Polynomial([0, 0, 0, 1, 2, 3])), str(polynomial.Polynomial([1, 2, 3])))
+
+    # [eq, adds, mult etc]
+    def test_eq(self):
+        self.assertTrue(polynomial.Polynomial([1,1,1]) == polynomial.Polynomial([1,1,1]))
+
+    def test_neq(self):
+        self.assertFalse(polynomial.Polynomial([1,1,1]) == polynomial.Polynomial([0,1,1]))
+
+    def test_addPolynomial(self):
+        self.assertEqual(polynomial.Polynomial([1,1,1]) + polynomial.Polynomial([1,1,1]), polynomial.Polynomial([2,2,2]))
+
 unittest.main()
+
+# p = polynomial.Polynomial([1,1,1,1,1,1,1])
+# for k,v in enumerate(p.coeffs):
+#     print(k)
