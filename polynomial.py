0553adcb2db109740a5e6ac50f0da9d2877e6bc9
Alexander Trifonov <alexander1.trifonov@intel.com>
2020-03-03 21:45:24 +0300

    fix radd, add with different length, empty obj change, fixes

--- polynomial.py
+++ polynomial.py
@@ -1,57 +1,66 @@
-#Load numpy and make optimization https://habr.com/ru/post/455722/
-
-
 class Polynomial:
     #self.coeffs = []
+
+    #Delete first N zeros as Polynomial([0,0,1,2]) same as Polynomial([1,2])
+    def fixZeros(self):
+        for k,v in enumerate(self.coeffs):  # 1 2 3 4 -> 1x^3 + 2x^2 + 3x + 4, but coeffs[0] = 4, coeffs[1] = 3, coeffs[i] = arg(x^i)
+            if v != 0:
+                del self.coeffs[:k]
+                break
+        if len(self.coeffs) == 0:   #Polinomial ALWAYS have 0 on the end? It's needed to be able to work with object even if it's empty. For example, to print polynomial, to __add__
+            self.coeffs.append(0)
+        return
+
+    def isEmpty(self):
+        return not len(self.coeffs) #0 - False, any other - True
 
     def __init__(self,_coeffs):
         self.coeffs = []
         if isinstance(_coeffs, (int)):
             self.coeffs.append(_coeffs)
             return
-        if not isinstance(_coeffs, (list, tuple, set, int)):
-            raise TypeError("Supported types for creating polynomial: list, tuple, set")
-        badZerosExists = True #remove first N zeros since they are usless, e.g. [0,0,0,1,2] -> x + 2 same as [1,2]
-        #index = _coeffs.index(not 0);
-        for v in _coeffs: #1 2 3 4 -> 1x^3 + 2x^2 + 3x + 4, but coeffs[0] = 4, coeffs[1] = 3, coeffs[i] = arg(x^i)
-            if(badZerosExists):
+        if isinstance(_coeffs, Polynomial):
+            for v in _coeffs:
+                self.coeffs[v] = _coeffs[v]
+            return
+        if not isinstance(_coeffs, (list, tuple)):
+            raise TypeError("Supported types for creating polynomial: list, tuple, int, Polynomial")
+        else:
+            for k,v in enumerate(_coeffs): #delete first N zeros
                 if v != 0:
-                    badZerosExists = False
-                    self.coeffs.append(int(v)) #checks whenever can cast to int or not
-                else:
-                    continue
-            else:
-                self.coeffs.append(int(v)) #checks whenever can cast to int or not
+                    self.coeffs = _coeffs[k:]
+                    return
+                if self.isEmpty():  # Polinomial ALWAYS have 0 on the end? It's needed to be able to work with object even if it's empty. For example, to print polynomial, to __add__
+                    self.coeffs.append(0)
 
     #1,2,3,4 = x^3 + 2x^2 + 3x + 4
     def __str__(self):
         text = ""
-        #print("text:" + text)
         #For X with max degree there are special rule to represent: "-2x^n_+_...". "-" without spaces
-
         if (len(self.coeffs) > 1):
             #x^n
             text = text + (("-" if self.coeffs[0] < 0 else "") + (str(abs(self.coeffs[0])) if abs(self.coeffs[0]) != 1 else "") + "x^" + str(len(self.coeffs) - 1) if self.coeffs[0] != 0 else "")
-            #print("text1:" + text)
             #[x^(n-1)..x^2]
             for k,v in enumerate(self.coeffs[1: -2]):
                 if v != 0:
                     text = text + (" - " if v < 0 else " + ") + (str(abs(v)) if abs(v) != 1 else "") + "x^"+str(len(self.coeffs)-k - 2)
-                    #print("text:"+k+" " + text)
             #x^1
             text = text + (((" + " if self.coeffs[-2] > 0 else " - ") + (str(abs(self.coeffs[-2])) if (abs(self.coeffs[-2]) != 1) else "") + "x") if self.coeffs[-2] != 0 else "")
             #x^0
             text = text + (((" + " if self.coeffs[-1] > 0 else " - ") + str(abs(self.coeffs[-1]))) if self.coeffs[-1] != 0 else "")
         else:
-            text = text + ((("" if self.coeffs[-1] > 0 else "-") + str(abs(self.coeffs[-1]))) if self.coeffs[-1] != 0 else "")
+            text = text + (("" if self.coeffs[0] >= 0 else "-") + str(abs(self.coeffs[0])))
         return text
 
     def __repr__(self):
         return "Polynomial("+str(self.coeffs)+")"
 
     def __eq__(self, other):
-        if not isinstance(other, Polynomial):
-            TypeError("Can't compare " + str(type(self)) + " with " + str(type(other)))
+        if not isinstance(other, Polynomial): #How can I check whenether can I cast type(other) to Polinomlial or not. Will it cost more (time/memory( than just checking and hardcoding?
+            if not isinstance(other, int):
+                raise TypeError("Can't compare " + str(type(self)) + " with " + str(type(other)))
+            else:
+                return self.coeffs[0] == other
         else:
             return self.coeffs == other.coeffs
 
@@ -59,11 +68,42 @@ class Polynomial:
         return len(self.coeffs)
 
     def __add__(self, other):
+        if not isinstance(other, (list, tuple, int, Polynomial)):
+            TypeError("Supported types to add: list, tuple, int, Polynomial")
         if isinstance(other, Polynomial):
-            if len(self) > len(other):
-                for k,v in enumerate(other.coeffs):
-                    self.coeffs[len(self.coeffs) - k - 1] += other.coeffs[len(self.coeffs) - k - 1] #since 1,2,3,4 means x^3 + 2x^2 + 3x^1 + 4 we start from the end
+            if self.isEmpty():
+                self.coeffs = other.coeffs[:]
+                return
+            if len(self) >= len(other):
+                for k,v in enumerate(reversed(other.coeffs)):
+                    self.coeffs[-(k+1)] += v
             else:
-                for k,v in enumerate(self.coeffs):
-                    self.coeffs[len(self.coeffs) - k - 1] += other.coeffs[len(self.coeffs) - k - 1]
+                for k,v in enumerate(reversed(self.coeffs)): #process same degree
+                    self.coeffs[-(k+1)] += other.coeffs[-(k+1)]
+                #copy new degree coeffs
+                self.coeffs[0:0] = other.coeffs[:len(other.coeffs) - len(self.coeffs)] #https://www.geeksforgeeks.org/python-insert-list-in-another-list/
+        elif isinstance(other, int):
+            if self.isEmpty():
+                self = Polynomial(other)
+                return
+            else:
+                self.coeffs[-1] += other
+        else:
+            TypeError("Error: how did you get here you cheeky bastard?")
+        self.fixZeros()
         return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        return
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    def __mul__(self, other):
+        return
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class PolynomialTests(unittest.TestCase):
     def test_supportedType(self):
         polynomial.Polynomial([1,2,3])
         polynomial.Polynomial((1, 2, 3))
-        polynomial.Polynomial({1,2,3})
+        #polynomial.Polynomial({1,2,3})
 
     # [repr and str]
     def test_negativeCoeffs(self):
@@ -22,8 +22,17 @@ class PolynomialTests(unittest.TestCase):
     def test_repr(self):
         self.assertEqual(repr(polynomial.Polynomial([1,2,-4])), "Polynomial([1, 2, -4])")
 
+    def test_boundMinZero(self):
+        self.assertEqual(str(polynomial.Polynomial(0)), "0")
+
+    def test_boundMinNegativeZero(self):
+        self.assertEqual(str(polynomial.Polynomial(-0)), "0")
+
     def test_boundMin(self):
         self.assertEqual(str(polynomial.Polynomial(1)), "1")
+
+    def test_boundMin2(self):
+        self.assertEqual(str(polynomial.Polynomial(6)), "6")
 
     def test_boundMinNegative(self):
         self.assertEqual(str(polynomial.Polynomial(-1)), "-1")
@@ -37,9 +46,6 @@ class PolynomialTests(unittest.TestCase):
     def test_zeroCoeffsMixed(self):
         self.assertEqual(str(polynomial.Polynomial([1, 0, 0, 4])), "x^3 + 4")
 
-    def test_boundMinZero(self):
-        self.assertEqual(str(polynomial.Polynomial(0)), "")
-
     def test_badZeros(self):
         self.assertEqual(str(polynomial.Polynomial([0, 0, 0, 1, 2, 3])), str(polynomial.Polynomial([1, 2, 3])))
 
@@ -50,11 +56,72 @@ class PolynomialTests(unittest.TestCase):
     def test_neq(self):
         self.assertFalse(polynomial.Polynomial([1,1,1]) == polynomial.Polynomial([0,1,1]))
 
+    def test_neqString(self):
+        with self.assertRaises(Exception):
+            polynomial.Polynomial(0) == "0"
+
+    def test_eqCoeff(self):
+        self.assertEqual(polynomial.Polynomial([1]), 1)
+
+    def test_eqCoeffSide(self):
+        self.assertEqual(1, polynomial.Polynomial([1]))
+
+    def test_initZeros(self):
+        self.assertEqual(polynomial.Polynomial([0,0,0]), polynomial.Polynomial(0))
+
     def test_addPolynomial(self):
-        self.assertEqual(polynomial.Polynomial([1,1,1]) + polynomial.Polynomial([1,1,1]), polynomial.Polynomial([2,2,2]))
+        self.assertEqual(polynomial.Polynomial([1,2,3]) + polynomial.Polynomial([1,1,1]), polynomial.Polynomial([2,3,4]))
+
+    def test_addPolynomialNegative(self):
+        self.assertEqual(polynomial.Polynomial([1,2,3,4]) + polynomial.Polynomial([1,0,-1,1]), polynomial.Polynomial([2,2,2,5]))
+
+    def test_addPolynomialDifferentLength(self):
+        self.assertEqual(polynomial.Polynomial([1, 2, 3, 4, 5]) + polynomial.Polynomial([1, 1, 2]), polynomial.Polynomial([1, 2, 4, 5, 7]))
+
+    def test_addPolynomialDifferentLength2(self):
+        self.assertEqual(polynomial.Polynomial([1, 2]) + polynomial.Polynomial([1, 2, 3]), polynomial.Polynomial([1, 3, 5]))
+
+    def test_addPolynomialDifferentLength3(self):
+        self.assertEqual(polynomial.Polynomial([1, 2, 3, 4]) + polynomial.Polynomial([-1, -2, 0, 0]), polynomial.Polynomial([3,4]))
+
+    def test_addPolynomialSameLength3(self):
+        self.assertEqual(polynomial.Polynomial([1, 2, 3]) + polynomial.Polynomial([-1, -2, 3]), polynomial.Polynomial([6]))
+
+    def test_addPolynomialZero(self):
+        self.assertEqual(polynomial.Polynomial([1,2,3]) + polynomial.Polynomial(0), polynomial.Polynomial([1,2,3]))
+
+    def test_addCoeff(self):
+        self.assertEqual(polynomial.Polynomial([1,1,1]) + 1, polynomial.Polynomial([1,1,2]))
+
+    def test_addCoef2(self):
+        self.assertEqual(polynomial.Polynomial([1]) + 1, polynomial.Polynomial(2))
+
+    def test_addCoeff3(self):
+        self.assertEqual(polynomial.Polynomial(0) + 0, 0)
+
+    def test_raddCoeff(self):
+        self.assertEqual(1 + polynomial.Polynomial([1,1,1]), polynomial.Polynomial([1,1,2]))
+
+    # def test_sub(self):
+    #     self.assertEqual(polynomial.Polynomial([2,2,2]) - polynomial.Polynomial([1,1,1]), polynomial.Polynomial([1,1,1]))
+    #
+    # def test_subCoeff(self):
+    #     self.assertEqual(polynomial.Polynomial([1, 1, 2]) - 1, polynomial.Polynomial([1, 1, 1]))
+    #
+    # def test_subCoef2(self):
+    #     self.assertEqual(polynomial.Polynomial([2]) - 1, polynomial.Polynomial(1))
+    #
+    # def test_subCoeff3(self):
+    #     self.assertEqual(polynomial.Polynomial(0) - 0, 0)
+    #
+    # def test_subCoeffSide(self):
+    #     self.assertEqual(1 - polynomial.Polynomial([1, 1, 1]), polynomial.Polynomial([1, 1, 0]))
+    #
+
 
 unittest.main()
 
+#print(repr(polynomial.Polynomial(0)))
 # p = polynomial.Polynomial([1,1,1,1,1,1,1])
 # for k,v in enumerate(p.coeffs):
 #     print(k)
